23a5b811a2c441ed28d6cf2668dd41d8d5d8bbc9
jphrubant <jphrubant@gmail.com>
2020-03-08 19:26:07 +0100

    safety commit

--- src/components/Map.js
+++ src/components/Map.js
@@ -20,7 +20,7 @@ class Map extends Component {
       pinVisible: false,
       newPin: null, // <---- array of coordinates
       allReports: [],
-      filter: null
+      // filter: ''
     };
   };
 
@@ -40,6 +40,19 @@ class Map extends Component {
     this.setState({newPin: event.lngLat, pinVisible})
   };
 
+  // handleFilterChange = event => {
+  //   const { value } = event.target;
+  //   this.setState({ filter : value });
+  //   console.log('FILTER', this.state)
+  // };
+
+  // handleFormSubmit = event => {
+  //   event.preventDefault();
+  //   const { value } = event.target;
+  //   this.setState({ filter : value });
+  //   console.log('FILTER', this.state)
+  // };
+
   render() {
     const {isLoggedIn} = this.props;
     return (
@@ -53,10 +66,8 @@ class Map extends Component {
           mapboxApiAccessToken={MAPBOX_TOKEN}
           onClick={this.mapClick}
         >
-
-
-        {
-          this.state.allReports.map(oneReport => {
+        
+        {this.state.allReports.map(oneReport => {
             return (
               <Marker key={oneReport._id} longitude={oneReport.location[0]} latitude={oneReport.location[1]}>
                 <img className="pin" src="./pin.png" alt="pin" />
@@ -72,17 +83,38 @@ class Map extends Component {
               latitude={this.state.newPin[1]}
               closeButton={false} 
               closeOnClick={false}>
-              {isLoggedIn ? (<Link to={`/create-report/?lng=${this.state.newPin[0]}&lat=${this.state.newPin[1]}`}>
-                <button className="popup-button">- Report -</button>
-              </Link>
+              {isLoggedIn ? (
+                <Link to={`/create-report/?lng=${this.state.newPin[0]}&lat=${this.state.newPin[1]}`}>
+                  <button className="popup-button">- Report -</button>
+                </Link>
               ) : (
-                <Link to="/login"><button className="popup-button">- Login or signup to create a report -</button></Link>
+                <Link to="/login">
+                  <button className="popup-button">- Login or signup to create a report -</button>
+                </Link>
               )}
             </Popup>)
             : null }
           <div>
             <p className="map-instruction"> Click the map to make a report</p>
           </div>
+
+          {/* <form onSubmit={this.handleFormSubmit}>
+            <select type="text"
+              name="filter"
+              value={''}
+              onChange={this.handleFilterChange}>
+                <option value=""> - Filter - </option>
+                <option value="Sexist">Sexist</option>
+                <option value="Racist">Racist</option>
+                <option value="Homophobic">Homophobic</option>
+                <option value="Transphobic">Transphobic</option>
+                <option value="Islamophobic">Islamophobic</option>
+                <option value="Antisemitic">Antisemitic</option>
+                <option value="Other">Other</option>
+              </select>
+              <button className="submit-button" type="submit">Filter</button>
+            </form> */}
+          
         </ReactMapGL>
       </div>
     );
